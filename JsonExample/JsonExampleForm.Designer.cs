e3ce096a49a54978144f5798b20bbe8fb208faa0
Cory LePage <cory@razorcx.com>
2017-12-18 09:49:31 -0800

    Renamed form

---
+++ JsonExample/JsonExampleForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace JsonExample
+{
+	partial class JsonExampleForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JsonExampleForm));
+			this.richTextBoxReadJson = new System.Windows.Forms.RichTextBox();
+			this.buttonReadJson = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonWriteJson = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// richTextBoxReadJson
+			// 
+			this.richTextBoxReadJson.Location = new System.Drawing.Point(13, 126);
+			this.richTextBoxReadJson.Name = "richTextBoxReadJson";
+			this.richTextBoxReadJson.Size = new System.Drawing.Size(350, 519);
+			this.richTextBoxReadJson.TabIndex = 0;
+			this.richTextBoxReadJson.Text = "";
+			// 
+			// buttonReadJson
+			// 
+			this.buttonReadJson.Location = new System.Drawing.Point(248, 79);
+			this.buttonReadJson.Name = "buttonReadJson";
+			this.buttonReadJson.Size = new System.Drawing.Size(115, 41);
+			this.buttonReadJson.TabIndex = 1;
+			this.buttonReadJson.Text = "Read Json";
+			this.buttonReadJson.UseVisualStyleBackColor = true;
+			this.buttonReadJson.Click += new System.EventHandler(this.buttonReadJson_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::JsonExample.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(159, 51);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// buttonWriteJson
+			// 
+			this.buttonWriteJson.Location = new System.Drawing.Point(13, 79);
+			this.buttonWriteJson.Name = "buttonWriteJson";
+			this.buttonWriteJson.Size = new System.Drawing.Size(115, 41);
+			this.buttonWriteJson.TabIndex = 9;
+			this.buttonWriteJson.Text = "Write Json";
+			this.buttonWriteJson.UseVisualStyleBackColor = true;
+			this.buttonWriteJson.Click += new System.EventHandler(this.buttonWriteJson_Click);
+			// 
+			// JsonExampleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(375, 657);
+			this.Controls.Add(this.buttonWriteJson);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.buttonReadJson);
+			this.Controls.Add(this.richTextBoxReadJson);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "JsonExampleForm";
+			this.Text = "Json Example";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBoxReadJson;
+		private System.Windows.Forms.Button buttonReadJson;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button buttonWriteJson;
+	}
+}
+
---
+++ JsonExample/JsonExampleForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonExample
+{
+	public partial class JsonExampleForm : Form
+	{
+		private readonly string _path = $"d:\\RazorCX\\Development\\Customer.json";
+
+		public JsonExampleForm()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonReadJson_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string jsonFromFile;
+				using (var reader = new StreamReader(_path))
+				{
+					jsonFromFile = reader.ReadToEnd();
+				}
+
+				richTextBoxReadJson.Text = jsonFromFile;
+
+				var customerFromJson = JsonConvert.DeserializeObject<Customer>(jsonFromFile);
+			}
+			catch (Exception ex)
+			{
+				// ignored
+			}
+		}
+
+		private void buttonWriteJson_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var customer = GetCustomer();
+
+				var jsonToWrite = JsonConvert.SerializeObject(customer, Formatting.Indented);
+
+				using (var writer = new StreamWriter(_path))
+				{
+					writer.Write(jsonToWrite);
+				}
+			}
+			catch (Exception ex)
+			{
+				// ignored
+			}
+		}
+
+		private Customer GetCustomer()
+		{
+			var customer = new Customer
+			{
+				Id = 1,
+				FirstName = "Bob",
+				LastName = "Smith",
+				Location = new Location
+				{
+					City = "Vancouver",
+					Province = "BC",
+					Country = "Canada"
+				},
+				Orders = new List<Order>
+				{
+					new Order
+					{
+						Id = 1,
+						Items = new List<Item>
+						{
+							new Item
+							{
+								Product = new Product
+								{
+									Sku = "34RTW89",
+									Name = "Turbocharger",
+									Price = 1050.00
+								},
+								Quantity = 3
+							},
+							new Item
+							{
+								Product = new Product
+								{
+									Sku = "14UPU90",
+									Name = "Hotrod",
+									Price = 987.00
+								},
+								Quantity = 4
+							},
+							new Item
+							{
+								Product = new Product
+								{
+									Sku = "11UPE70",
+									Name = "Barn",
+									Price = 1902.00
+								},
+								Quantity = 1
+							}
+						}
+					}
+				}
+			};
+
+			return customer;
+		}
+	}
+
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Location Location { get; set; }
+		public List<Order> Orders { get; set; }
+	}
+
+	public class Location
+	{
+		public string City { get; set; }
+		public string Province { get; set; }
+		public string Country { get; set; }
+	}
+
+	public class Order
+	{
+		public int Id { get; set; }
+		public List<Item> Items { get; set; }
+	}
+
+	public class Item
+	{
+		public Product Product { get; set; }
+		public int Quantity { get; set; }
+	}
+
+	public class Product
+	{
+		public string Sku { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+	}
+}
